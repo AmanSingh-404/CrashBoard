2b8b4ff6bca180265b5416497359a91902107fc8
Aman Singh <amanscode2005@gmail.com>
2026-05-26 09:09:55 +0530

    update instance and socket url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,19 +1,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 })
 
-// automatically attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('cb_token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-// if token expires, redirect to login
 api.interceptors.response.use(
   (response) => response,
   (error) => {
